50358e940238f7380b272e50a3169c87d191a18a
azmiagr <159001181+azmiagr@users.noreply.github.com>
2024-03-15 16:59:34 +0700

    post.go

--- entity/post.go
+++ entity/post.go
@@ -3,7 +3,7 @@ package entity
 import "github.com/google/uuid"
 
 type Post struct {
-	ID          uint   `json:"id" gorm:"type:varchar(36);primary_key;"`
+	ID          uint  `json:"id" gorm:"primary_key;autoIncrement"`
 	Title       string `json:"title" gorm:"type:varchar(255);not null;"`
 	Description string `json:"description" gorm:"type:text;not null;"`
 	User        User   `json:"-"`
