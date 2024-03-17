76b81700f9b867052181558ca10f9d6c3a2a40d6
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2024-03-17 13:32:04 +0700

    cors update

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -1,14 +1,15 @@
 package middleware
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"time"
 )
 
 func (m *middleware) Cors() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowAllOrigins:  true,
 		AllowMethods:     []string{"*"},
 		AllowHeaders:     []string{"*"},
 		ExposeHeaders:    []string{"*"},
